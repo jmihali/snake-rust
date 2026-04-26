22e9af643423f8434f6b4c8b81244ebcf1791ddb
Joan Mihali <jmihali1997@gmail.com>
2026-04-26 15:43:21 +0200

    feat: implement error handling for snake

---
+++ src/game/snake/mod.rs
@@ -0,0 +1,8 @@
+// region:    --- Modules
+
+mod error;
+mod snake_impl;
+
+// -- Flatten
+pub use error::*;
+pub use snake_impl::*;
---
+++ src/game/snake/snake_impl.rs
@@ -0,0 +1,92 @@
+use std::collections::{VecDeque, vec_deque};
+
+use crate::game::coordinates::Coordinates;
+use crate::game::orientation::{self, Orientation};
+use crate::game::segment::Segment;
+use crate::game::snake::{Error, Result};
+
+pub struct Snake {
+    // in the body, the first segment is the tail,
+    // and the last segment is the head
+    body: VecDeque<Segment>,
+}
+
+impl Snake {
+    pub fn new(tail_coordinates: Coordinates, orientation: Orientation, length: u32) -> Self {
+        let body = VecDeque::from([Segment::new(tail_coordinates, orientation, length)]);
+        Snake { body: body }
+    }
+}
+
+impl Snake {
+    fn remove_tail_segment(&mut self) -> Result<()> {
+        let tail = self.body.pop_front();
+        if tail.is_none() {
+            return Err(Error::SnakeBodyEmpty);
+        }
+        Ok(())
+    }
+
+    fn add_head_segment(&mut self, orientation: Orientation) -> Result<()> {
+        let current_head_segment = self.body.back();
+
+        match current_head_segment {
+            Some(head) => {
+                let new_head_segment_tail = head.get_head_coordinates();
+                let new_head_segment = Segment::new(new_head_segment_tail, orientation, 0);
+                self.body.push_back(new_head_segment);
+                Ok(())
+            }
+            None => Err(Error::SnakeBodyEmpty),
+        }
+    }
+
+    fn shorten_tail_segment(&mut self) -> Result<()> {
+        match self.body.front_mut() {
+            Some(tail) => {
+                tail.shorten_tail();
+                Ok(())
+            }
+            None => Err(Error::SnakeBodyEmpty),
+        }
+    }
+
+    fn get_tail_length(&self) -> Result<u32> {
+        match self.body.front() {
+            Some(tail) => Ok(tail.get_length()),
+            None => Err(Error::SnakeBodyEmpty),
+        }
+    }
+
+    fn extend_head_segment(&mut self) -> Result<()> {
+        match self.body.back_mut() {
+            Some(head) => Ok(head.extend_head()),
+            None => Err(Error::SnakeBodyEmpty),
+        }
+    }
+
+    pub fn get_head_orientation(&self) -> Result<Orientation> {
+        match self.body.back() {
+            Some(head) => Ok(head.get_orientation()),
+            None => Err(Error::SnakeBodyEmpty),
+        }
+    }
+
+    pub fn set_head_orientation(&mut self, orientation: Orientation) -> Result<()> {
+        if orientation != self.get_head_orientation()? {
+            self.add_head_segment(orientation)?;
+        }
+        Ok(())
+    }
+
+    pub fn advance(&mut self, grow: bool) -> Result<()> {
+        self.extend_head_segment()?;
+        if !grow {
+            self.shorten_tail_segment()?;
+            if self.get_tail_length()? == 0 {
+                self.remove_tail_segment()?;
+            }
+        }
+        Ok(())
+    }
+}
