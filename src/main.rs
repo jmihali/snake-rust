050b8220cdb948ec726f043b8139fa636f8e0f1f
Joan Mihali <jmihali1997@gmail.com>
2026-05-11 17:02:00 +0200

    chore: add conditional compilation for macroquad and raylib

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 mod game_loop;
-mod platform_macroquad;
-// mod platform_raylib;
-
 use game_loop::GameLoop;
+
+#[cfg(feature = "macroquad_support")]
+mod platform_macroquad;
+#[cfg(feature = "macroquad_support")]
 use platform_macroquad::MacroquadPlatform;
-// use platform_raylib::RaylibPlatform;
-// use tokio;
+
+#[cfg(feature = "raylib_support")]
+mod platform_raylib;
+#[cfg(feature = "raylib_support")]
+use platform_raylib::RaylibPlatform;
+#[cfg(feature = "raylib_support")]
+use tokio::runtime::Runtime;
 
 const CELL_SIZE: f32 = 20.0;
 const GRID_WIDTH: u32 = 20;
@@ -15,10 +21,13 @@ const MOVE_DELAY: f32 = 0.15; // seconds between moves
 #[macroquad::main("Snake in Rust")]
 // #[tokio::main(flavor = "current_thread")]
 async fn main() {
+    #[cfg(feature = "macroquad_support")]
     let platform = MacroquadPlatform::new();
-    // let platform = RaylibPlatform::new(800, 800, "Snake Game - Raylib Edition");
+
+    #[cfg(feature = "raylib_support")]
+    let platform = RaylibPlatform::new(800, 800, "Snake Game - Raylib Edition");
 
     let mut game_loop = GameLoop::new(GRID_WIDTH, GRID_HEIGHT, CELL_SIZE, MOVE_DELAY, platform);
 
-    game_loop.run_game_loop().await;
+    let _ = game_loop.run_game_loop().await;
 }
