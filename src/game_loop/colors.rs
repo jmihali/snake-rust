011c4b142cd33a0325b1cf4cd803534f8b5ccf21
Joan Mihali <jmihali1997@gmail.com>
2026-05-14 18:44:42 +0200

    refactor: split game loop implementation into multiple files

---
+++ src/game_loop/colors.rs
@@ -0,0 +1,8 @@
+use crate::game_loop::platform::MyColor;
+
+pub(crate) const RED: MyColor = [1.0, 0.0, 0.0, 1.0];
+pub(crate) const GREEN: MyColor = [0.0, 1.0, 0.0, 1.0];
+pub(crate) const DARKGREEN: MyColor = [0.0, 0.4, 0.0, 1.0];
+pub(crate) const DARKGRAY: MyColor = [0.3, 0.3, 0.3, 1.0];
+pub(crate) const BLACK: MyColor = [0.0, 0.0, 0.0, 1.0];
+pub(crate) const WHITE: MyColor = [1.0, 1.0, 1.0, 1.0];
--- src/game_loop/mod.rs
+++ src/game_loop/mod.rs
@@ -1,12 +1,15 @@
 // region:    --- Modules
+mod colors;
 mod error;
 mod game_core;
+mod game_loop_decl;
 mod game_loop_impl;
+mod game_loop_impl_priv;
 mod platform;
 mod snake_algorithm;
 
 // -- Flatten
 pub use game_core::*;
-pub use game_loop_impl::*;
+pub use game_loop_decl::*;
 pub use platform::*;
 pub use snake_algorithm::*;
